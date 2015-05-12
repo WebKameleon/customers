b661ca75375b1cf0f271a7605d9ccb0c45a3712e
Karol Huszal <karol@vps105410.ovh.net>
2015-05-12 13:40:21 +0200

    byuleco

--- jabadabadoo/template/1/js/main.js
+++ jabadabadoo/template/1/js/main.js
@@ -215,6 +215,13 @@ $('a.button-col-hotel').click(function(){
     window.open('http://form.jotformpro.com/form/51124013104938?nazwaHotelu='+name+'&termin='+termin, 'blank','scrollbars=yes,toolbar=no,width=700,height=800');
 });
 
+$('button.ask').click(function(){
+    var name=encodeURIComponent($('p.place-name a').html());
+    var termin=encodeURIComponent($('p.d_from').text());
+    
+    window.open('http://form.jotformpro.com/form/51124013104938?nazwaHotelu='+name+'&termin='+termin, 'blank','scrollbars=yes,toolbar=no,width=700,height=800');
+});
+
 
 
 $(document).ready(function() {
