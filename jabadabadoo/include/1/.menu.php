e2380524271dbb38e896737a1a61e3a910f583b5
Piotr Podstawski <piotr.podstawski@gammanet.pl>
2015-05-28 11:50:03 +0200

    przeminęło z wiatrem

--- jabadabadoo/include/1/.menu.php
+++ jabadabadoo/include/1/.menu.php
@@ -23,9 +23,11 @@
             
             if (isset($link['d_from']) && $link['d_from'])
             {
+
                 $weblink2=new weblinkModel($link['sid']);
                 $weblink2->alt=$link['nego']?:date('d.m',strtotime($link['d_from'])) .' - '. date('d.m',strtotime($link['d_to']));
                 $weblink2->name=$this->webpage['title'];
+                $weblink2->hidden = strtotime($link['d_from'])>time() || (strtotime($link['d_to'])>time() && !$link['confirm']) ? 0 : 1;
                 $weblink2->save();
             }
         }
---
+++ jabadabadoo/include/1/menu.php
@@ -0,0 +1,3 @@
+<?php
+
+    if (is_object($this)) include __DIR__.'/.menu.php';
