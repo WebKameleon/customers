faa4cf74049b38d88aff9eb72e1be51be6dcbbeb
Piotr Podstawski <piotr.podstawski@gammanet.pl>
2015-06-03 12:26:40 +0200

    ok

--- aesa/include/tools/point.php
+++ aesa/include/tools/point.php
@@ -1,5 +1,32 @@
 <?php
+    function get ($d,$a){
+        foreach ($a AS $k) if (isset($d[$k])) return $d[$k];
+    }
+    $pointTypes = [ "oua", "ppo", "spo", "road", "parking", "hotel", "wc", "restaurant", "gas", "cafe" ];
+
+    
     $data=json_decode(file_get_contents($argv[1]),1);
     
-    print_r($data);
+    foreach($data AS $d) 
+    {
+        $rec=[];
+        
+        $rec['name']=get($d,['namePl','name']);
+        $cords=get($d,['coords','cordinates']);
+        $rec['lat']=$cords[0];
+        $rec['lng']=$cords[1];
+        $desc=get($d,['description']);
+        $rec['pl']=get($d,['contentPl']);
+        $rec['en']=get($d,['contentEn']);
+        if (!$rec['pl'] && is_array($desc)) {
+            $rec['pl']=$desc[0];
+            $rec['en']=$desc[1];
+        }
+        foreach ($pointTypes AS $t) $rec[$t]=false;
+        $pattern=get($d,['pattern']);
+        if (is_array($patern)) {
+            foreach ($patern AS $k=>$b) if ($b) $rec[$k]=true;
+        }
+    }
     
+    print_r($rec);
