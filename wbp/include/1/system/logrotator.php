92adfc7ed24338ab0700b2e6d965746b4470afb0
Gold Webkameleon <gold@webkameleon.c.webkameleon-gold.internal>
2016-02-09 11:15:36 +0000

    ok

--- wbp/include/1/system/logrotator.php
+++ wbp/include/1/system/logrotator.php
@@ -1,14 +1,16 @@
 <?php
 
-
 use google\appengine\api\cloud_storage\CloudStorageTools;
 
 require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
-$base='gs://'.CloudStorageTools::getDefaultGoogleStorageBucketName().'/posts';
+$base='gs://'.CloudStorageTools::getDefaultGoogleStorageBucketName().'/post';
+
+echo '<pre>';
+echo "$base\n";
 
 foreach (scandir($base) AS $component)
 {
-    echo "Starting $component<br/>";
+    echo "Starting $component\n";
     if ($component[0]=='.') continue;
     $dir="$base/$component";
     
@@ -17,18 +19,22 @@ foreach (scandir($base) AS $component)
     foreach(scandir($dir) AS $f)
     {
         if ($f[0]=='.') continue;
-	if (substr($f,0,10)==date('Y-m-d') continue;
-	if (strlen($f)==14) continue;
+	if (substr($f,0,10)==date('Y-m-d')) continue;
+	if (strlen($f)<15) continue;
 
-	$log[substr($f,0,10)].=$f."\n".file_get_contents("$dir/$f")."\n\n";
+	if (!isset($log[substr($f,0,10)])) $log[substr($f,0,10)]=''; 
+	$log[substr($f,0,10)].=$f."\n".file_get_contents("$dir$f")."\n\n";
 	
-	
-        //unlink("$dir/$f");
+        unlink("$dir$f");
     }
+
 
 	foreach ($log AS $f=>$c)
 	{
-		file_put_contents($dir.'/'.$f,$c);
+		if (strlen($f)<10) continue;
+		file_put_contents($dir.$f.'.txt',$c);
 	}
+
+
 	
 }
