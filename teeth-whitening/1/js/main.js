a53b140a0c40d65bb34a8255eb476ce1c447cd64
Edi Webkameleon <cmseditor@editor.c.webkameleon-gold.internal>
2017-01-20 18:26:40 +0000

    zmiany

--- teeth-whitening/1/js/main.js
+++ teeth-whitening/1/js/main.js
@@ -3,9 +3,8 @@ $(document).ready(function () {
     setTimeout(function(){
         $(document).trigger('scroll');
         
-        
         $('.info-block .img-wr img,.info-block_mod .img-wr img').each(function(){
-            $(this).parent().parent().css({
+            $(this).closest('div.info-block').css({
                 'padding-bottom':0,
                 'height':$(this).height()+'px'
             });
