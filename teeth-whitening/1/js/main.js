41dfcbaa9301e3611f18c1952ec353731ff798d9
Edi Webkameleon <cmseditor@editor.c.webkameleon-gold.internal>
2017-02-15 15:58:21 +0000

    loader-iframe

--- teeth-whitening/1/js/main.js
+++ teeth-whitening/1/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
         $(document).trigger('scroll');
         
         $('.info-block .img-wr img,.info-block_mod .img-wr img').each(function(){
+		if ($(this).height()<300) return;
             $(this).closest('div.info-block').css({
                 'padding-bottom':0,
                 'height':$(this).height()+'px'
