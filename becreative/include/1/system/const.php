c10a1520e98c49a1c58ab7d9a379f86081011644
Edi Webkameleon <cmseditor@editor.c.webkameleon-gold.internal>
2017-10-24 16:55:25 +0000

    googleauth

--- becreative/include/1/system/const.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$JOTFORM_API_KEY='2abb959823a891bd6d91c1fbee5c2a1f';
-
-	
--- teeth-whitening/1/js/main.js
+++ teeth-whitening/1/js/main.js
@@ -1,4 +1,33 @@
 
+var userStat = function(u) {
+	if (!u.id) {
+		$('.googlelogout').fadeOut(800);
+		$('.googlelogin > a').each(function(){
+			var tag=$(this).attr('tag');
+			if (tag) $(this).html(tag);
+		});
+		$('.googlelogin > a').click(function(){
+			WebKameleonAuth.GoogleAuth(userStat);		
+			return false;
+		});
+	} else {
+		$('.googlelogout').fadeIn(500);
+		$('.googlelogin > a').each(function(){
+			$(this).attr('tag',$(this).text());
+			$(this).html(u.first_name);
+		});
+		$('.googlelogout > a').click(function(){
+			WebKameleonAuth.GoogleLogout(userStat);		
+			return false;
+		});
+	}
+	
+}
+
+function googleauth() {
+	WebKameleonAuth.GoogleUser(userStat);
+}
+
 $(document).ready(function () {
     setTimeout(function(){
         $(document).trigger('scroll');
@@ -14,5 +43,15 @@ $(document).ready(function () {
     
 	$('.box-transp .img-wr').fadeOut(500);    
     },50);
+
+	var login=$("li.googlelogin,a.googlelogin");
+
+	if (login.length>0) {
+		var s = document.createElement("script");
+		s.type = "text/javascript";
+		s.src = "http://auth.webkameleon.com/client.js?callback=googleauth";	
+		$("head").append(s);
+	}
+
     
 })
