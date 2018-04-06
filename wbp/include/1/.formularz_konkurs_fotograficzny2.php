d5edfe637c164754e4f29a40d09074fd72789ed3
Piotr Podstawski <piotr@webkameleon.com>
2018-04-06 10:01:50 +0000

    nowy formularz dla wladka

---
+++ wbp/include/1/.formularz_konkurs_fotograficzny2.php
@@ -0,0 +1,1 @@
+<?php include __DIR__.'/.formularz_konkurs_fotograficzny.php';
--- wbp/include/1/ajax/contest.php
+++ wbp/include/1/ajax/contest.php
@@ -1,6 +1,8 @@
 <?php
     ini_set('display_errors',true);
     
+    //if (rand(0,2)+0==0) myd988die();
+    
     if (isset($_SERVER['SERVER_SOFTWARE']) && strstr(strtolower($_SERVER['SERVER_SOFTWARE']),'engine')) {
         require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
     }
@@ -29,21 +31,17 @@
     
     $teraz=date('Y-m-d,H:i');
     
-    $data['id'] = md5($data['id'].$data['surname']);
+    if (isset($data['id'])) $data['id'] = md5($data['id'].$data['surname']);
     $data['payment']='';
     
-    $sid=$data['sid'];
-    unset($data['sid']);
+    if (isset($data['sid'])) {
+        $sid=$data['sid'];
+        unset($data['sid']);
+        $td_data=WBP::get_data($sid);
+    } else die();
     
-    $td_data=WBP::get_data($sid);
     
-    if (isset($data['files'])) {
-        $img_info=$data['files'];
-        unset($data['files']);
-    } else {
-        $img_info=[];
-    }
-
+    
     
     $data_to_write_to_spreadsheet=array();
     $response_images=array();
@@ -61,10 +59,17 @@
     $done_uri=dirname($done_uri).'/contest-done.php';
     
     
-    //if (isset($_GET['cookie'])) session_id($_GET['cookie']);
+    @session_start();
     
-    @session_start();       
-
+    $img_info = isset($_SESSION['img_infos']) ? $_SESSION['img_infos'] : [];
+    
+    if (isset($data['files'])) {
+        $img_info=$data['files'];
+        foreach($data['files'] AS $k=>$v) $img_info[$k]=$v;
+        unset($data['files']);
+        $_SESSION['img_infos'] = $img_info;
+    }
+    
 
     if (isset($_SESSION['contest'])) $contest=$_SESSION['contest'];
     else {
@@ -73,7 +78,8 @@
     $contest['data']=$_REQUEST;
     if (isset($contest['data']['files'])) unset($contest['data']['files']);
     
-    foreach ($_REQUEST['files'] AS $k=>$f) {
+    //$_REQUEST['files']
+    foreach ($_FILES AS $k=>$f) {
         $contest['files'][$k]=$f;
     }
     $_SESSION['contest']=$contest;
@@ -203,6 +209,7 @@
         $index=$f['name'][$lp];
         $type=$f['type'][$lp];
         
+        
         //if (strstr($f['name'][$lp],'canon2')) mydd3ie($f);
         
         $lp++;
@@ -210,76 +217,78 @@
         
         @session_write_close();
         
-        $row=array('date'=>date('d-m-Y H:i:s'));
-        $row=array_merge($row,$data,$img_info[$index]);
+        if (isset($img_info[$index])) {
+            
+        
+            $row=array('date'=>date('d-m-Y H:i:s'));
+            $row=array_merge($row,$data,$img_info[$index]);
+        
+            $kat=isset($img_info[$index]['category']) ? $img_info[$index]['category'] : '';
     
-        $kat=isset($img_info[$index]['category']) ? $img_info[$index]['category'] : '';
-
-        if ($kat)
-        {
-            $kategorie=Google::getFileChildren($parent_id,$kat);
-    
-            if (!count($kategorie['items']))
+            if ($kat)
             {
-                sleep(1);
                 $kategorie=Google::getFileChildren($parent_id,$kat);
-                if (!count($kategorie['items'])) {
-                    $kategoria=Google::createFolder($kat,$parent_id);
-                    $parent_id=$kategoria['id'];                    
-                } else {
+        
+                if (!count($kategorie['items']))
+                {
+                    sleep(1);
+                    $kategorie=Google::getFileChildren($parent_id,$kat);
+                    if (!count($kategorie['items'])) {
+                        $kategoria=Google::createFolder($kat,$parent_id);
+                        $parent_id=$kategoria['id'];                    
+                    } else {
+                        $parent_id=$kategorie['items'][0]['id'];
+                    }
+                    
+    
+                }
+                else
+                {
                     $parent_id=$kategorie['items'][0]['id'];
                 }
+            }
+            
+            
+            $filename=WBP::str_to_url($data['surname']).'-'.WBP::str_to_url($data['name']).'-'.WBP::str_to_url($row['title']).WBP::str_to_url($row['setno']).'-'.$index;
+            
+            $row['filename']=$filename;
+            
+            $gfile=Google::uploadFile($filename,$type,$blob,$parent_id);
+            $blob='';
+        
+            if (isset($gfile['id']))
+            {
+                $thumbnailUrl=preg_replace('/[0-9]+$/','80',$gfile['thumbnailLink']);
+                $row['thumbnail']=$thumbnailUrl;
+                $row['phone']=str_replace('+48','',$row['phone']);
+                $row['phone']=str_replace('+','0',$row['phone']);
+                $row['description']=str_replace("\r",'',$row['description']);
+                $row['description']=str_replace("\n",' ',$row['description']);
                 
-
+                $data_to_write_to_spreadsheet[]=$row;
+                $url=dirname($_SERVER['REQUEST_URI']).'/contest-bckd.php?sid='.$sid.'&id='.$gfile['id'];
+                
+                $response_images[]=array(
+                    'name'=>$index,
+                    'size'=>$size,
+                    'type'=>$type,
+                    'url'=>$url,
+                    'thumbnailUrl'=>$thumbnailUrl,
+                    'deleteUrl'=>$url,
+                    'deleteType'=>'DELETE',
+                    'id'=>$sid.$data['id'],
+                    'done'=>$done_uri
+                );
             }
             else
             {
-                $parent_id=$kategorie['items'][0]['id'];
+                contest_ret(array('files'=>array(),'debug'=>$debug,'gfile'=>$gfile));    
             }
         }
-        
-        
-        $filename=WBP::str_to_url($data['surname']).'-'.WBP::str_to_url($data['name']).'-'.WBP::str_to_url($row['title']).WBP::str_to_url($row['setno']).'-'.$index;
-        
-        $row['filename']=$filename;
-        
-        $gfile=Google::uploadFile($filename,$type,$blob,$parent_id);
-        $blob='';
-    
-        if (isset($gfile['id']))
-        {
-            $thumbnailUrl=preg_replace('/[0-9]+$/','80',$gfile['thumbnailLink']);
-            $row['thumbnail']=$thumbnailUrl;
-            $row['phone']=str_replace('+48','',$row['phone']);
-            $row['phone']=str_replace('+','0',$row['phone']);
-            $row['description']=str_replace("\r",'',$row['description']);
-            $row['description']=str_replace("\n",' ',$row['description']);
-            
-            $data_to_write_to_spreadsheet[]=$row;
-            $url=dirname($_SERVER['REQUEST_URI']).'/contest-bckd.php?sid='.$sid.'&id='.$gfile['id'];
-            
-            $response_images[]=array(
-                'name'=>$index,
-                'size'=>$size,
-                'type'=>$type,
-                'url'=>$url,
-                'thumbnailUrl'=>$thumbnailUrl,
-                'deleteUrl'=>$url,
-                'deleteType'=>'DELETE',
-                'id'=>$sid.$data['id'],
-                'done'=>$done_uri
-            );
-        }
-        else
-        {
-            contest_ret(array('files'=>array(),'debug'=>$debug,'gfile'=>$gfile));    
-        }
-        
     }
     
     
     
-    @session_write_close();
     WBP::dumpJson($sid.$data['id'].'-'.rand(200000,time()).'.json',[
         'response_images' => $response_images,
         'td_data' => $td_data,
@@ -287,61 +296,5 @@
     ]);
     
     
-    // Spreadsheet part 
-    /*
-    Spreadsheet::setToken(null);
-    Spreadsheet::setToken($_SESSION['spreadsheets_access_token']);
-
-    //$debug['td_data']=$td_data;
-    
-    $sheets=Spreadsheet::listWorksheets($td_data['drive']['id']);
-    
-    if (!is_array($sheets)) contest_ret(array('files'=>array(),'debug'=>$debug,'sheets'=>$sheets));
-    
-    //$debug['sheets']=$sheets;
-    
-    $worksheet_id=null;
-    foreach ($sheets AS $id=>$contents)
-    {
-        if ($contents['title']==$td_data['title'])
-        {
-            $worksheet_id=$id;
-            break;
-        }
-    }
-    
-    if (!$worksheet_id)
-    {
-        $worksheet=Spreadsheet::addWorksheet($td_data['drive']['id'],$td_data['title']);
-        $id=$worksheet->id;
-        $worksheet_id=end(explode('/',$worksheet->id));
-    }
-    
-    if ($debug) $debug['rows']=array();
-    foreach ($data_to_write_to_spreadsheet AS $row)
-    {
-        
-        $header=Spreadsheet::getWorksheet($td_data['drive']['id'],$worksheet_id,'max-row=1');
-        if (isset($header[0])) $header=$header[0];
-        
-        if ($debug) $debug['rows'][]=$row;
-        if ($debug) $debug['header']=$header;
-        
-        foreach ($row AS $k=>$v)
-        {
-            if (!in_array($k,$header))
-            {
-                @Spreadsheet::update_cell($td_data['drive']['id'],$worksheet_id,0,count($header),$k);
-                $header[]=$k;
-            }
-        }
-        
-        
-        $row=@Spreadsheet::addListRow($td_data['drive']['id'],$worksheet_id,$row);
-
-
-    }
-    
-    */
 
     contest_ret(array('files'=>$response_images,'debug'=>$debug,'get'=>$_GET,'ssid'=>session_id()));
