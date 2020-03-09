53d4f600d866789146755c3c5f2f60a1de376d09
Piotr Podstawski <piotr@webkameleon.com>
2020-03-09 10:48:05 +0000

    dataMod

--- wbp/include/1/system/.dataModyfikacji.php
+++ wbp/include/1/system/.dataModyfikacji.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'modyfikacja: '.Tools::date($this->webpage['nd_update']);
+$webtd=new webtdModel();
+$all=$webtd->getAll(array($page));
+$update=0;
+for ($i=0; $i<count($all); $i++)
+  if ($all[$i]['nd_update'] > $update)
+    $update=$all[$i]['nd_update'];
+
+
+echo 'modyfikacja: '.Tools::date($update);
