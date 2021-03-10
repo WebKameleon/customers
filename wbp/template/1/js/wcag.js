66a7cf008b029ac30cf06fc284e024b77158423b
Piotr Podstawski <piotr@webkameleon.com>
2021-03-10 08:34:35 +0000

    wcag

--- wbp/template/1/js/wcag.js
+++ wbp/template/1/js/wcag.js
@@ -1,7 +1,13 @@
-$('.skip-main').attr('tabindex',1);
-$('.gsc-input input').attr('tabindex',2);
-let tabindex=3;
 
-$('.main-menu>ul.navbar-nav>li>a').each (function(){
-    $(this).attr('tabindex',tabindex++);
-})
+function wcag_tabindex() {
+    $('.skip-main').attr('tabindex',1);
+    $('.gsc-input input').attr('tabindex',2);
+    let tabindex=3;
+    
+    $('.main-menu>ul.navbar-nav>li>a').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+}
+
+setTimeout(wcag_tabindex,2000);
+$(document).ready(wcag_tabindex):
