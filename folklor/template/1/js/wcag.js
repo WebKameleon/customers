df81d1c27c5c1908e4ab4f746d2c7eb54242e79f
Piotr Podstawski <piotr@webkameleon.com>
2021-03-24 14:51:59 +0000

    wcag

---
+++ folklor/template/1/js/wcag.js
@@ -0,0 +1,83 @@
+
+function wcag_tabindex() {
+    let tabindex=1;
+
+    $('.skip-main').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+
+    $('.logo img').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+    
+    $('.gsc-input input').attr('tabindex',tabindex++).attr('title','Wyszukiwarka w serwisie');
+    
+    $('.kmw_langs a').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+    $('.wcag button').attr('tabindex',tabindex++);
+    
+    
+    $('#navbar>ul.navbar-nav>li>a').each (function(){
+        $(this).attr('tabindex',tabindex++);
+
+	
+        $(this).parent().find('li').each(function(){
+            $(this).attr('tabindex',tabindex++);
+        });
+    });
+
+    $('#banner .bx-viewport li').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+
+    $('#sidebar .article').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+
+    
+    
+    $('.article-list').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+    
+    
+    $('.article .kmw_article_content').each (function(){
+        $(this).attr('tabindex',tabindex++);
+        
+        $(this).find('iframe').each(function(){
+        	$(this).attr('tabindex',tabindex++);
+        });
+        
+    });
+    
+    $('.gallery2 figure').each (function(){
+        $(this).attr('tabindex',tabindex++);
+        
+    });
+    
+    
+
+    $('#wbp_shop_results li').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+    
+     $('aside #side-nav .panel a').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+    
+    //footer
+    $('#footer p').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+    $('#footer .menu-simple .img-list').each (function(){
+        $(this).attr('tabindex',tabindex++);
+    });
+    
+    
+   
+
+}
+
+setTimeout(wcag_tabindex,10000);
+$(document).ready(wcag_tabindex);
